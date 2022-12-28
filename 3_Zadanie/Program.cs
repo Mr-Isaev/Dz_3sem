d7f327dfaf7b2f41a0cf6279115ccab97b59d25b
«Alexandr» <>
2022-12-29 02:17:53 +0500

    Решение третьей задачи

--- 3_Zadanie/Program.cs
+++ 3_Zadanie/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+Console.Write("Введите любое число--> ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+	Console.Write(" " + Math.Pow(i, 3));
+	i++;
+}
